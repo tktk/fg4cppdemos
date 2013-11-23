833aa823a439bad57ca9c4f3597eaf84c8d59a50
tekuto <g.tekuto@gmail.com>
2013-11-23 17:44:06 +0900

    以下のデモの生成ルール、ソースファイルを追加 ・window_mousebuttonevent

--- wscripts/demos/__init__.py
+++ wscripts/demos/__init__.py
@@ -6,6 +6,7 @@ from . import window_closeevent
 from . import window_paintevent
 from . import window_positionevent
 from . import window_sizeevent
+from . import window_mousebuttonevent
 
 def build( _context ):
     helloworld.build( _context )
@@ -14,3 +15,4 @@ def build( _context ):
     window_paintevent.build( _context )
     window_positionevent.build( _context )
     window_sizeevent.build( _context )
+    window_mousebuttonevent.build( _context )
