b0e01509ede81a5b5db644fdd16415c61b166543
tekuto <g.tekuto@gmail.com>
2013-11-23 15:54:18 +0900

    window_painteventの生成ルールを追加 window_painteventのソースファイルを追加

--- wscripts/demos/__init__.py
+++ wscripts/demos/__init__.py
@@ -3,8 +3,10 @@
 from . import helloworld
 
 from . import window_closeevent
+from . import window_paintevent
 
 def build( _context ):
     helloworld.build( _context )
 
     window_closeevent.build( _context )
+    window_paintevent.build( _context )
