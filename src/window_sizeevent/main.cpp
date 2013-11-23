754aa2a19d066eb9cbf02416d679b40372b48877
tekuto <g.tekuto@gmail.com>
2013-11-23 16:44:12 +0900

    以下のデモの生成ルール、ソースファイルを追加 ・window_positionevent ・window_sizeevent

---
+++ src/window_sizeevent/main.cpp
@@ -0,0 +1,174 @@
+﻿#include "gf/util/export.h"
+#include "gf4cpp/window/eventhandlers.h"
+#include "gf4cpp/window/window.h"
+#include "gf4cpp/string/utf32.h"
+#include "gfpp/common/memory.h"
+#include "gfpp/common/primitives.h"
+
+#include <cstdio>
+#include <mutex>
+#include <condition_variable>
+
+enum
+{
+    WIDTH = 640,
+    HEIGHT = 480,
+};
+
+const auto  TITLE = U"window_sizeevent";
+
+void waitEnd(
+    const gf::Bool &            _ENDED
+    , std::mutex &              _mutex
+    , std::condition_variable & _cond
+)
+{
+    std::unique_lock< std::mutex >  lock( _mutex );
+
+    _cond.wait(
+        lock
+        , [
+            &_ENDED
+        ]
+        {
+            return _ENDED;
+        }
+    );
+}
+
+void notifyEnd(
+    gf::Bool &                  _ended
+    , std::mutex &              _mutex
+    , std::condition_variable & _cond
+)
+{
+    std::unique_lock< std::mutex >  lock( _mutex );
+
+    _ended = true;
+
+    _cond.notify_one();
+}
+
+gf::WindowEventHandlers * newWindowEventHandlers(
+    gf::Bool &                  _ended
+    , std::mutex &              _mutex
+    , std::condition_variable & _cond
+)
+{
+    auto    eventHandlersUnique = gf::unique( gf::newWindowEventHandlers() );
+    if( eventHandlersUnique.get() == nullptr ) {
+        std::printf( "gf::newWindowEventHandlers()が失敗\n" );
+
+        return nullptr;
+    }
+    auto &  eventHandlers = *eventHandlersUnique;
+
+    gf::setCloseEventHandler(
+        eventHandlers
+        , [
+            &_ended
+            , &_mutex
+            , &_cond
+        ]
+        (
+            gf::Window &
+        )
+        {
+            notifyEnd(
+                _ended
+                , _mutex
+                , _cond
+            );
+        }
+    );
+
+    gf::setSizeEventHandler(
+        eventHandlers
+        , [](
+            gf::Window &
+            , gf::Int       _width
+            , gf::Int       _height
+        )
+        {
+            std::printf(
+                "gf::Window size[ %d x %d ]\n"
+                , _width
+                , _height
+            );
+        }
+    );
+
+    return eventHandlersUnique.release();
+}
+
+gf::Window * newWindow(
+    gf::Bool &                  _ended
+    , std::mutex &              _mutex
+    , std::condition_variable & _cond
+)
+{
+    auto    eventHandlersUnique = gf::unique(
+        newWindowEventHandlers(
+            _ended
+            , _mutex
+            , _cond
+        )
+    );
+    if( eventHandlersUnique.get() == nullptr ) {
+        return nullptr;
+    }
+    auto &  eventHandlers = *eventHandlersUnique;
+
+    auto    titleUnique = gf::unique(
+        gf::newUtf32( TITLE )
+    );
+    if( titleUnique.get() == nullptr ) {
+        std::printf( "gf::newUtf32()が失敗\n" );
+
+        return nullptr;
+    }
+    auto &  title = *titleUnique;
+
+    auto    windowUnique = gf::unique(
+        gf::newWindow(
+            eventHandlers
+            , title
+            , WIDTH
+            , HEIGHT
+        )
+    );
+    if( windowUnique.get() == nullptr ) {
+        std::printf( "gf::newWindow()が失敗\n" );
+
+        return nullptr;
+    }
+
+    return windowUnique.release();
+}
+
+GFEXPORT gf::Int main(
+)
+{
+    gf::Bool                ended = false;
+    std::mutex              mutex;
+    std::condition_variable cond;
+
+    auto    windowUnique = gf::unique(
+        newWindow(
+            ended
+            , mutex
+            , cond
+        )
+    );
+    if( windowUnique.get() == nullptr ) {
+        return 1;
+    }
+
+    waitEnd(
+        ended
+        , mutex
+        , cond
+    );
+
+    return 0;
+}
--- wscripts/demos/__init__.py
+++ wscripts/demos/__init__.py
@@ -4,9 +4,13 @@ from . import helloworld
 
 from . import window_closeevent
 from . import window_paintevent
+from . import window_positionevent
+from . import window_sizeevent
 
 def build( _context ):
     helloworld.build( _context )
 
     window_closeevent.build( _context )
     window_paintevent.build( _context )
+    window_positionevent.build( _context )
+    window_sizeevent.build( _context )
