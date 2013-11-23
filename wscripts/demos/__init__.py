131bfadec8d718aabdfae82b612c03db74743668
tekuto <g.tekuto@gmail.com>
2013-11-23 17:59:16 +0900

    以下のデモの生成ルール、ソースファイルを追加 ・window_closerequest

--- wscripts/demos/__init__.py
+++ wscripts/demos/__init__.py
@@ -7,6 +7,7 @@ from . import window_paintevent
 from . import window_positionevent
 from . import window_sizeevent
 from . import window_mousebuttonevent
+from . import window_closerequest
 
 def build( _context ):
     helloworld.build( _context )
@@ -16,3 +17,4 @@ def build( _context ):
     window_positionevent.build( _context )
     window_sizeevent.build( _context )
     window_mousebuttonevent.build( _context )
+    window_closerequest.build( _context )
