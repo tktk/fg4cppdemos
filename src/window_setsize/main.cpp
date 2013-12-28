2fe5dc31aa684e4a393b5f89f71beac631585479
tekuto <g.tekuto@gmail.com>
2013-12-28 14:23:42 +0900

    window_setsizeにfgのヘッダファイル配置の変更に伴う修正を適用

--- src/window_setsize/main.cpp
+++ src/window_setsize/main.cpp
@@ -1,4 +1,7 @@
 ﻿#include "fg/util/export.h"
+#include "fgpp/def/main/main.h"
+#include "fg/def/main/main.h"
+#include "fg4cpp/def/main/args.h"
 #include "fg4cpp/window/closeevent.h"
 #include "fg4cpp/window/sizeevent.h"
 #include "fg4cpp/window/mousebuttonevent.h"
@@ -6,7 +9,6 @@
 #include "fg4cpp/window/window.h"
 #include "fg4cpp/string/utf32.h"
 #include "fgpp/common/memory.h"
-#include "fgpp/common/primitives.h"
 
 #include <cstdio>
 #include <mutex>
@@ -193,7 +195,8 @@ fg::Window * newWindow(
     return windowUnique.release();
 }
 
-FGEXPORT fg::Int main(
+fg::Int fgMain(
+    const fg::Args &
 )
 {
     fg::Bool                ended = false;
@@ -218,4 +221,13 @@ FGEXPORT fg::Int main(
     );
 
     return 0;
+}
+
+FGEXPORT FgInt fgMain(
+    const FgArgs *  _ARGS
+)
+{
+    return fgMain(
+        fg::toFgpp( *_ARGS )
+    );
 }
